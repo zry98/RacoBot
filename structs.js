84e5c65c0b06e404ae484a7f423adae566dc88c9
zry98 <git@zry.io>
2021-09-01 22:06:42 +0200

    Fix struct fields validation

--- structs.js
+++ structs.js
@@ -3,8 +3,10 @@ import { FIBAPILoginRedirectBaseURL } from './constants'
 // UserInfo represents a user's information API response
 // Endpoint: /jo
 function UserInfo(o) {
-  if (!o || !o.nom || !o.cognoms || !o.email) {
-    throw new Error('[FIB API] Invalid UserInfo response')
+  for (const field of ['nom', 'cognoms', 'email']) {
+    if (!(field in o)) {
+      throw new Error('[FIB API] Invalid UserInfo response')
+    }
   }
   return {
     username: o.username,
@@ -15,8 +17,10 @@ function UserInfo(o) {
 
 // Notice represents a single notice in a NoticesResponse API response
 function Notice(o) {
-  if (!o.id || !o.titol || !o.codi_assig || !o.text || !o.data_insercio || !o.data_modificacio || !o.data_caducitat || !o.adjunts) {
-    throw new Error('[FIB API] Invalid Notices response')
+  for (const field of ['id', 'titol', 'codi_assig', 'text', 'data_insercio', 'data_modificacio', 'data_caducitat', 'adjunts']) {
+    if (!(field in o)) {
+      throw new Error('[FIB API] Invalid Notice')
+    }
   }
   return {
     id: o.id,
@@ -32,8 +36,10 @@ function Notice(o) {
 
 // Attachment represents a single attachment in a Notice's attachments
 function Attachment(o) {
-  if (!o.tipus_mime || !o.nom || !o.url || !o.data_modificacio || !o.mida) {
-    throw new Error('[FIB API] Invalid Notices response')
+  for (const field of ['tipus_mime', 'nom', 'url', 'data_modificacio', 'mida']) {
+    if (!(field in o)) {
+      throw new Error('[FIB API] Invalid Attachment')
+    }
   }
   return {
     mimeTypes: o.tipus_mime,
@@ -50,8 +56,10 @@ function Attachment(o) {
 // Notices represents a user's notices API response
 // Endpoint: /jo/avisos
 function Notices(o) {
-  if (!o || !o.count || !o.results) {
-    throw new Error('[FIB API] Invalid Notices response')
+  for (const field of ['count', 'results']) {
+    if (!(field in o)) {
+      throw new Error('[FIB API] Invalid Notices response')
+    }
   }
   return o.results.map(notice => new Notice(notice))
 }
